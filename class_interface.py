673a0c6220486c7b45c29510d96ea8f1c3f2b869
Шумилин Иван <mirovengil@gmail.com>
2021-07-10 00:33:43 +0300

    Добавлен шаблон pygame.

---
+++ class_interface.py
@@ -0,0 +1,40 @@
+'''
+Модуль, что отвечает за отрисовку пользовательского интерфейса.
+'''
+
+import pygame
+
+#SVGA разрешение. Сделать круче не позволяют устройство и религия.
+WINDOW_SIZE_X = 800
+WINDOW_SIZE_Y = 600
+
+def get_img_by_name(name):
+    '''
+    Возвращает пригодную для pygame картинку с названием name : str.
+    Картинка должна быть расположена в директории ./img и должна иметь
+    расширение .png.
+    '''
+    return pygame.image.load('./img/' + name + '.png').convert_alpha()
+
+DATA = dict()
+
+def load_images(name):
+    '''
+    Создаёт словарь DATA и заполняет его массивом картинок .png из папки ./img/, сохраняя
+    названия ('./img/image.png' будет доступно по ключу 'image').
+    Все необходимые ключи надо указать в файле ./<name>.txt
+    '''
+    fin = open('./' + name + '.txt', 'r' )
+    fin.close()
+    DATA = dict()
+    
+
+if __name__ == "__main__":
+    DATA = load_images('img_list')
+    pygame.init()
+    screen = pygame.display.set_mode((WINDOW_SIZE_X, WINDOW_SIZE_Y))
+    game_over = False
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
